9e82a703b84f3e941da0f28ab0211fa395fcd105
Chris Williams <chris.d.williams@gmail.com>
2016-01-17 10:26:40 -0500

    Initial set of refactoring to extract out the hard coded values in to the .env file using the dotenv package and configuration file to allow the application to run as a service on Linux.

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-var DASH_MAC_ADDRESS = "PLACE_AMAZON_DASH_HERE";
-var LIFX_API_TOKEN = "PLACE_LIFX_API_TOKEN_HERE";
+var dotenv = require('dotenv').config({path: '/home/pi/dash/.env'});
+ 
+var dash_button = require('node-dash-button');  
+var dash = dash_button(process.env.DASH_MAC_ADDRESS);
 
-var dash_button = require('node-dash-button');
-var dash = dash_button(DASH_MAC_ADDRESS);
+var lifxObj = require('lifx-api');  
+var lifx = new lifxObj(process.env.LIFX_API_TOKEN);
 
-var lifxObj = require('lifx-api');
-var lifx = new lifxObj(LIFX_API_TOKEN);
+var light_label = process.env.LIFX_LIGHT_LABEL;
+   
+console.log("Waiting for Dash press for " + light_label + "...");
+dash.on("detected", function (){  
+    console.log("Dash press detected for " + light_label);
+    lifx.listLights('label:' + light_label, function(res) {
+        var jsonRes = JSON.parse(res);
+        console.log(jsonRes);
+        var id = jsonRes.id; //using LIFX bulb ID
 
-dash.on("detected", function (){
-	lifx.listLights('all', function(res) {
-		var jsonRes = JSON.parse(res);
-		var id = jsonRes[0].id; //using LIFX bulb ID
-
-		lifx.togglePower(id, function(res) {
-			console.log(res);
-		});
-	});
+        lifx.togglePower(id, function(res) {
+            console.log(res);
+        });
+    });
 });
